d790856becfddd96c9c3ed791e1b7f5d47ad4fe8
mingpepe <mingpepemath@gmail.com>
2023-12-11 20:37:22 +0800

    feat: use element 'movie_player' to sync setting with <video>

--- content.js
+++ content.js
@@ -3,41 +3,42 @@ console.log('code in content.js');
 function insertPlaybackRate() {
     console.log('insertPlaybackRate');
     const logo = document.getElementById('logo-icon');
-    const video = document.querySelector('video');
+    const player = document.getElementById('movie_player');
     if (!logo) {
         console.log('logo not found, insert playbackRate fail');
         return;
     }
-    if (!video) {
-        console.log('video not found, insert playbackRate fail');
+    if (!player) {
+        console.log('player not found, insert playbackRate fail');
         return;
     }
     const label = document.createElement('label');
     logo.appendChild(label);
 
     setInterval(() => {
-        label.textContent = video.playbackRate.toFixed(1);
+        label.textContent = player.getPlaybackRate().toFixed(1);
     }, 1000);
 }
 
 function onDOMContentLoaded() {
     console.log('DOMContentLoaded');
-    const video = document.querySelector('video');
-    if (video) {
+    const player = document.getElementById('movie_player');
+    if (player) {
         document.addEventListener('keydown', (event) => {
+            const playbackRate = player.getPlaybackRate();
             if (event.key === 'w') {
-                video.playbackRate = (video.playbackRate + 0.1) < 16 ? video.playbackRate + 0.1 : 16;
+                player.setPlaybackRate((playbackRate + 0.1) < 16 ? playbackRate + 0.1 : 16);
             } else if (event.key === 'q') {
-                video.playbackRate = (video.playbackRate - 0.1) > 1 ? video.playbackRate - 0.1 : 1;
+                player.setPlaybackRate((playbackRate - 0.1) > 1 ? playbackRate - 0.1 : 1);
             } else if (event.key == 'p') {
-                video.playbackRate = (video.playbackRate + 0.5) < 16 ? video.playbackRate + 0.5 : 16;
+                player.setPlaybackRate((playbackRate + 0.5) < 16 ? playbackRate + 0.5 : 16);
             } else if (event.key == 'o') {
-                video.playbackRate = (video.playbackRate - 0.5) > 1 ? video.playbackRate - 0.5 : 1;
+                player.setPlaybackRate((playbackRate - 0.5) > 1 ? playbackRate - 0.5 : 1);
             }
         });
         setTimeout(insertPlaybackRate, 3000);
     } else {
-        console.log('video not found');
+        console.log('player not found');
     }
 }
 
