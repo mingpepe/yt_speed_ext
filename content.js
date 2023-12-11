4e4d953a8a4a9e5124837c130bd53275b7fab3dc
mingpepe <mingpepemath@gmail.com>
2023-12-11 20:40:02 +0800

    fix: get element after amount of time

--- content.js
+++ content.js
@@ -18,28 +18,24 @@ function insertPlaybackRate() {
     setInterval(() => {
         label.textContent = player.getPlaybackRate().toFixed(1);
     }, 1000);
+
+    document.addEventListener('keydown', (event) => {
+        const playbackRate = player.getPlaybackRate();
+        if (event.key === 'w') {
+            player.setPlaybackRate((playbackRate + 0.1) < 16 ? playbackRate + 0.1 : 16);
+        } else if (event.key === 'q') {
+            player.setPlaybackRate((playbackRate - 0.1) > 1 ? playbackRate - 0.1 : 1);
+        } else if (event.key == 'p') {
+            player.setPlaybackRate((playbackRate + 0.5) < 16 ? playbackRate + 0.5 : 16);
+        } else if (event.key == 'o') {
+            player.setPlaybackRate((playbackRate - 0.5) > 1 ? playbackRate - 0.5 : 1);
+        }
+    });
 }
 
 function onDOMContentLoaded() {
     console.log('DOMContentLoaded');
-    const player = document.getElementById('movie_player');
-    if (player) {
-        document.addEventListener('keydown', (event) => {
-            const playbackRate = player.getPlaybackRate();
-            if (event.key === 'w') {
-                player.setPlaybackRate((playbackRate + 0.1) < 16 ? playbackRate + 0.1 : 16);
-            } else if (event.key === 'q') {
-                player.setPlaybackRate((playbackRate - 0.1) > 1 ? playbackRate - 0.1 : 1);
-            } else if (event.key == 'p') {
-                player.setPlaybackRate((playbackRate + 0.5) < 16 ? playbackRate + 0.5 : 16);
-            } else if (event.key == 'o') {
-                player.setPlaybackRate((playbackRate - 0.5) > 1 ? playbackRate - 0.5 : 1);
-            }
-        });
-        setTimeout(insertPlaybackRate, 3000);
-    } else {
-        console.log('player not found');
-    }
+    setTimeout(insertPlaybackRate, 3000);
 }
 
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
