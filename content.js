187cbade872d3f098bb4e79a3e0724c6bbb13578
mingpepe <mingpepemath@gmail.com>
2023-12-11 20:46:48 +0800

    feat: handle playbackRate larger than 2

--- content.js
+++ content.js
@@ -4,6 +4,7 @@ function insertPlaybackRate() {
     console.log('insertPlaybackRate');
     const logo = document.getElementById('logo-icon');
     const player = document.getElementById('movie_player');
+    const video = document.querySelector('video');
     if (!logo) {
         console.log('logo not found, insert playbackRate fail');
         return;
@@ -12,23 +13,34 @@ function insertPlaybackRate() {
         console.log('player not found, insert playbackRate fail');
         return;
     }
+    if (!video) {
+        console.log('video not found, insert playbackRate fail');
+        return;
+    }
     const label = document.createElement('label');
     logo.appendChild(label);
 
     setInterval(() => {
-        label.textContent = player.getPlaybackRate().toFixed(1);
+        label.textContent = video.playbackRate.toFixed(1);
     }, 1000);
 
     document.addEventListener('keydown', (event) => {
-        const playbackRate = player.getPlaybackRate();
+        const playbackRate = video.playbackRate;
+        let value = null;
         if (event.key === 'w') {
-            player.setPlaybackRate((playbackRate + 0.1) < 16 ? playbackRate + 0.1 : 16);
+            value = (playbackRate + 0.1) < 16 ? playbackRate + 0.1 : 16;
         } else if (event.key === 'q') {
-            player.setPlaybackRate((playbackRate - 0.1) > 1 ? playbackRate - 0.1 : 1);
+            value = (playbackRate - 0.1) > 1 ? playbackRate - 0.1 : 1;
         } else if (event.key == 'p') {
-            player.setPlaybackRate((playbackRate + 0.5) < 16 ? playbackRate + 0.5 : 16);
+            value = (playbackRate + 0.5) < 16 ? playbackRate + 0.5 : 16;
         } else if (event.key == 'o') {
-            player.setPlaybackRate((playbackRate - 0.5) > 1 ? playbackRate - 0.5 : 1);
+            value = (playbackRate - 0.5) > 1 ? playbackRate - 0.5 : 1;
+        }
+        if (value) {
+            player.setPlaybackRate(value);
+            if (value > 2) {
+                video.playbackRate = value;
+            }
         }
     });
 }
